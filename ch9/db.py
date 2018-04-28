c5a941810c96ba9863e4e95f20648dcbfc60cbbd
CHANG FU CHIAO <zector1030@gmail.com>
2018-04-28 21:42:33 +0800

    ch9: create database automaticaly

--- ch9/db.py
+++ ch9/db.py
@@ -1,17 +1,25 @@
 import os
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, text
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 Base = declarative_base()
-DB_URI = os.getenv('DB_URI', 'sqlite:///')
-DB_URI='mysql+pymysql://root:pa55word@127.0.0.1/ch6'
-DB_URI='postgresql://postgres:pa55@127.0.0.1:5432/ch6'
+DB_USER='postgres'
+DB_PASS='pa55'
+DB_HOST='127.0.0.1'
+DB_PORT='5432'
+DB_NAME='ch9'
+DB_ENGINE='postgresql://{}:{}@{}:{}'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT)
+DB_URI='postgresql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 print('connect db {}'.format(DB_URI))
 
+engine = create_engine(DB_ENGINE, isolation_level='AUTOCOMMIT')
+engine.execute("DROP DATABASE IF EXISTS {}".format(DB_NAME))
+engine.execute("CREATE DATABASE {}".format(DB_NAME))
 engine = create_engine(DB_URI)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
+
 
 def reset_db():
     from models.store import StoreModel
@@ -20,12 +28,4 @@ def reset_db():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-if __name__=='__main__':
-    # e.g. env DB_URI=sqlite:///item.db python db.py
-    print('drop and create {}'.format(DB_URI))
-    from models.store import StoreModel
-    from models.item import ItemModel
-    from models.user import UserModel
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
     
